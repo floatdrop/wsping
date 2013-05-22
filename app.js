c1d2776f78409b2f598dcf0f585cccf755195aa2
Vsevolod Strukchinsky <floatdrop@gmail.com>
2013-05-22 17:11:28 +0600

    working

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-
-var express = require('express'), socketio = require('socket.io');
-
-var app = express.createServer();
-var io = socketio.listen(app);
+var express = require('express'),
+	app = express(),
+	http = require('http'),
+	server = http.createServer(app),
+	io = require('socket.io').listen(server);
+var port = process.env.PORT || 80;
 
 app.use(express.static(__dirname + '/public'));
-app.listen(80);
+
+server.listen(port);
+
+console.log("Server started at http://localhost:" + port);
+
 io.set('log level', 3);
 
-io.sockets.on('ping', function (socket) {
-  socket.emit('pong', (new Date()).getTime());
+io.sockets.on('connection', function (socket) {
+	socket.on('message', function () {
+		socket.emit('pong', (new Date()).getTime());
+	});
+	socket.on('disconnect', function () {});
 });
